e372ce3632175b8558665703e4df5a368177ca70
Damien Buttler <damien@doublehops.com>
2014-03-23 22:24:52 +1100

    Added a little style to the charts.

--- views/market/_chart.php
+++ views/market/_chart.php
@@ -1,1 +1,4 @@
-<div id="chart-<?= $market->id ?>" class="chart-container" style="width:100%; height:400px;" data-market-name="<?= $market->label ?>" data-id="<?= $market->id ?>" data-time="last7Days"></div>
+<div class="market-container">
+    <h3><?= $market->primary_name ?> / <?= $market->secondary_name ?></h3>
+    <div id="chart-<?= $market->id ?>" class="chart-container" style="width:100%; height:350px;" data-market-name="<?= $market->label ?>" data-id="<?= $market->id ?>" data-time="last7Days"></div>
+</div>
