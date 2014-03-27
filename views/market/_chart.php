9b937f073409198f49f5aa0356ce0998214cdfb2
Damien Buttler <damien@doublehops.com>
2014-03-27 21:26:06 +1100

    Charts now showing last 30 days.

--- views/market/_chart.php
+++ views/market/_chart.php
@@ -1,5 +1,5 @@
 <div class="market-container">
     <div class="mask"></div>
     <h3><?= $market->primary_name ?> / <?= $market->secondary_name ?></h3>
-    <div id="chart-<?= $market->id ?>" class="chart-container" style="width:100%; height:350px;" data-market-name="<?= $market->label ?>" data-id="<?= $market->id ?>" data-time="last7Days"></div>
+    <div id="chart-<?= $market->id ?>" class="chart-container" style="width:100%; height:350px;" data-market-name="<?= $market->label ?>" data-id="<?= $market->id ?>" data-time="last30Days"></div>
 </div>
