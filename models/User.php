582f5bf8d76bcbe97236feb0a95fbfe8e4d4a0fb
Damien Buttler <damien@doublehops.com>
2014-05-02 11:17:10 +1000

    Changed admin password.

--- models/User.php
+++ models/User.php
@@ -14,7 +14,7 @@ class User extends \yii\base\Object implements \yii\web\IdentityInterface
 		'100' => [
 			'id' => '100',
 			'username' => 'admin',
-			'password' => 'admin',
+			'password' => 'nomate',
 			'authKey' => 'test100key',
 			'accessToken' => '100-token',
 		],
