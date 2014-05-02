6515a96be6dc3f8cbf68f3921494ff4579048c69
Damien Buttler <damien@doublehops.com>
2014-05-03 00:59:39 +1000

    Added items to main page index.

--- views/site/index.php
+++ views/site/index.php
@@ -10,7 +10,17 @@ $this->title = 'Coin Hawk';
 
 	<div class="body-content">
 
-        <?= Html::a('Cryptsy Full Listing', \Yii::$app->getUrlManager()->createUrl(['/market/full-listing', 'id' => 1])) ?>
+        <h2>Site Index</h2>
+        <ul>
+            <li><?= Html::a('Cryptsy Markets', \Yii::$app->getUrlManager()->createUrl('/market/index')) ?></li>
+            <li><?= Html::a('Cryptsy Full Chart Listing (CPU intensive)', \Yii::$app->getUrlManager()->createUrl(['/market/full-listing', 'id' => 1])) ?></li>
+            <li>---------------------------------------------------------------------------------------------------------</li>
+            <li><?= Html::a('LTC/BTC', \Yii::$app->getUrlManager()->createUrl(['/market/view', 'id' => 99])) ?></li>
+            <li><?= Html::a('RDD/BTC', \Yii::$app->getUrlManager()->createUrl(['/market/view', 'id' => 149])) ?></li>
+            <li><?= Html::a('DOGE/BTC', \Yii::$app->getUrlManager()->createUrl(['/market/view', 'id' => 73])) ?></li>
+            <li><?= Html::a('DRK/BTC', \Yii::$app->getUrlManager()->createUrl(['/market/view', 'id' => 74])) ?></li>
+            <li><?= Html::a('BC/BTC', \Yii::$app->getUrlManager()->createUrl(['/market/view', 'id' => 162])) ?></li>
+        </ul>
 
 	</div>
 </div>
